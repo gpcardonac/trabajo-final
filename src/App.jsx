533607347fb1f70f4eb3217b0d78a15e43f1180a
gpcardonac <gpcardonac@gmail.com>
2023-03-11 18:14:47 -0500

    Se inserta la primera tarea en component Appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import {TodoList} from './components/TodoList';
+
+
 export function App() {
-    return <div>Hola Mundo!!!!</div>;
+    return <TodoList todos={[
+        { id: 1, task: 'Tarea 1', completed: false}
+    ]} />;
 }
