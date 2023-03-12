a4d973fca401e3b51595b17e0bf019610cb5aa26
gpcardonac <gpcardonac@gmail.com>
2023-03-11 19:58:00 -0500

    Se inserta botones de tarea nueva tarea y eliminar tarea

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from "react";
-import {TodoList} from './components/TodoList';
+import React, { Fragment, useState } from "react";
+import {TodoList} from "./components/TodoList";
 
 
 export function App() {
-    return <TodoList todos={[
+    const [todos, setTodos] = useState([
+
         { id: 1, task: 'Tarea 1', completed: false}
-    ]} />;
+    ]);
+    return (
+        <Fragment>
+    <TodoList todos={todos} />
+    <input type="text" placeholder="Nueva Tarea"/>
+    <button>+</button>   
+    <button>Elimiar</button> 
+        
+        </Fragment>
+    );
 }
